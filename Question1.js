05c30fda13b7e8ef61bb1cd1c7e069817cc7bcc1
pankulbhardwaj-byte <pankulbhardwaj@gmail.com>
2026-04-14 22:46:34 +0530

    Question1.js

--- Question1.js
+++ Question1.js
@@ -1,38 +1,36 @@
-Question 1 
+function isPrime(n){
+    if (n<2) return false;
 
-let L = parseInt(prompt("Enter L:"));
-let R = parseInt(prompt("Enter R:"));
-let K = parseInt(prompt("Enter K:"));
-let finalCount = 0;
-function checkPrime(num) {
-    if (num <= 1) {
-        return false;
+    for (let i=2;i*i<=n;i++){
+        if(n%i===0) return false;
     }
-    let isPrime = true;
-    for (let i = 2; i < num; i++) {
-        if (num % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-    return isPrime;
+    return true;
 }
-for (let i = L; i <=R; i++) {
-    if (i % K == 0) {
-        let strNum = i.toString();
-        let hasZero = false;
-        let sumOfDigits = 0;
-        for (let j = 0; j < strNum.length; j++) {
-            if (strNum[j] == '0') {
-                hasZero = true;
-            }
-            sumOfDigits = sumOfDigits + parseInt(strNum[j]);
-        }
-        if (hasZero == false) {
-            if (checkPrime(sumOfDigits) == true) {
-                finalCount++;
-            }
-        }
-    }
+function containsZero(x){
+    return x.toString().includes('0');
 }
-alert(finalCount);
+function digitSum(x){
+    let sum=0;
+    let str=x.toString();
+    for(let q of str){
+        sum+=Number(q);
+    }
+    return sum;
+}
+function isValid(x,K) {
+    return x%K===0 && !containsZero(x) && isPrime(digitSum(x));
+}
+function countValid(L,R,K){
+    let count=0;
+    for(let i=L;i<=R;i++){
+        if(isValid(i,K)) count++;
+    }
+    return count;
+}
+
+let L=Number(prompt("Enter L:"));
+let R=Number(prompt("Enter R:"));
+let K=Number(prompt("Enter K:"));
+
+let result=countValid(L,R,K);
+alert("Result: "+result);
